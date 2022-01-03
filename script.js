1eb7b02bcb9da93a23a9fb505cd3f7a66a708094
Ethan Crompton <ethan.crompton1@gmail.com>
2022-01-03 18:09:39 +0000

    weather api call, added basic weather for manchester, not yet dynamic to user

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ var dayLeng = 10;
 var trimmedDay = day.substring(0, dayLeng);
 
 cDate.innerHTML = trimmedDay;
+
+var weatherKey = "24c0f9ca369c9bf518cb17089064cc19";
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Manchester&units=metric&appid=" + weatherKey,
+        type: "GET",
+        success: function (result){
+            console.log(result);
+            weatherResult(result);
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    })
+
+    function weatherResult(data){
+        try {
+            $("#cTemp").html(Math.round(data.main.temp) + "°");
+            $("#cCity").html(data.name + ", " + data.sys.country);
+        } catch (error) {
+            $("#weatherBox").html("Error Caught: " + error);
+        }
+    }
